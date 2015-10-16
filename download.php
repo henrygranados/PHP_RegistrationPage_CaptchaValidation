97612dcddca4fbf4533e3f3dcb24fe2cc1cf1bd7
Henry Granados <henrygranados03@gmail.com>
2015-10-16 01:30:55 -0600

    added download.php file

---
+++ download.php
@@ -0,0 +1,31 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<style type="text/css">
+h2{
+color:#4cae4c;
+}
+</style>
+<?php
+echo "<h2>Your information has been sent successfully!</h2><br/>";
+echo "<table style = 'width:600px;'class='table table-striped' border = '1'>";
+echo "<thead>";
+echo "<tr>";
+echo "<th>Firstname</th>";
+echo "<th>Name</th>";
+echo "<th>Gender</th>";
+echo "</tr>";
+echo "</thead>";
+echo "<tr>";
+echo "<td>";
+echo $_GET['fname'];
+echo "</td>";
+echo "<td>";
+echo $_GET['lname'];
+echo "</td>";
+echo "<td>";
+echo $_GET['radio'];
+echo "</td>";
+echo "</tr>";
+echo "</table>";
