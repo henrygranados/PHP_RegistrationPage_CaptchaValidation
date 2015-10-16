cf75e41e1541fb76b967815d193b33db23acd20c
Henry Granados <henrygranados03@gmail.com>
2015-10-16 01:31:59 -0600

    added new file

---
+++ submit.php
@@ -0,0 +1,27 @@
+<?php
+function msg($status,$txt)
+{
+    return '{"status":'.$status.',"txt":"'.$txt.'"}';
+}
+// we check if everything is filled in and perform checks
+//check if fname is empty
+if(!$_POST['fname'])
+{
+    die(msg(0,"<p>Please enter your first name.</p>"));
+    $nameErr = "Name is required";
+}
+//check if lname is empty
+if(!$_POST['lname'])
+{
+    die(msg(0,"<p>Please choose a gender</p>"));
+}
+
+//Check if gender is checked
+if(!isset($_POST['radio']))
+{
+ die(msg(0,"<p>Please choose a gender</p>"));
+}
+
+
+header('Location:download.php?fname='.$_POST['fname']."&lname=".$_POST['lname']."&radio=".$_POST['radio']);
+?>
